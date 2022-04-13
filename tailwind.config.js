db5ec7caae4af50d33e9f7327f2edbcdb7154373
Bach Ly <bly@cochlear.com>
2022-04-13 15:05:25 +1000

    Add pod with list

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,60 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    cursor: {
+      auto: 'auto',
+      default: 'default',
+      pointer: 'pointer',
+      wait: 'wait',
+      'not-allowed': 'not-allowed',
+      'zoom-in': 'zoom-in',
+      'zoom-out': 'zoom-out'
+    },
+    opacity: {
+      '0': '0',
+      '10': '.1',
+      '20': '.2',
+      '30': '.3',
+      '40': '.4',
+      '50': '.5',
+      '60': '.6',
+      '70': '.7',
+      '80': '.8',
+      '90': '.9',
+      '95': '.95',
+      '100': '1',
+    },
+    extend: {
+      maxWidth: {
+        '8xl': '90rem',
+        '9xl': '96rem',
+      },
+      screens: {
+        'print': { 'raw': 'print' }
+      },
+      spacing: {
+        "1/1": "100%",
+        "1/2": "50%",
+        "1/3": "33.333333%",
+        "2/3": "66.666667%",
+        "1/4": "25%",
+        "2/4": "50%",
+        "3/4": "75%",
+        "1/5": "20%",
+        "2/5": "40%",
+        "3/5": "60%",
+        "4/5": "80%",
+        "9/16": "56.25%",
+        "10/9": "110%"
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      scale: ['group-hover'],
+      display: ['group-hover'],
+      opacity: ['disabled'],
+    }
   },
   plugins: [],
 }

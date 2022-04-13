637d8efac22243cec70dac5a85c1c202b43af081
Bach Ly <bly@cochlear.com>
2022-04-13 13:22:12 +1000

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import 'tailwindcss/tailwind.css'
+import '../styles/main.scss'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Component {...pageProps} />
+  );
+}
+
+export default MyApp
